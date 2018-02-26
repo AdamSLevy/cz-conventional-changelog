09cd30783bb4ba657ad55d3f80eb372eb635dd71
Adam S Levy <adam@aslevy.com>
2018-02-26 00:32:01 -0900

    feat(line-length): Set subject max length 120 and wrap length to 79

--- engine.js
+++ engine.js
@@ -39,7 +39,7 @@ module.exports = function (options) {
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
     prompter: function(cz, commit) {
-      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+      console.log('\nLine 1 will be cropped at 120 characters. All other lines will be wrapped after 72 characters.\n');
 
       // Let's ask some questions of the user
       // so that we can populate our commit
@@ -93,7 +93,8 @@ module.exports = function (options) {
         }
       ]).then(function(answers) {
 
-        var maxLineWidth = 100;
+        var maxLineWidth = 72;
+        var maxSubjectWidth = 120;
 
         var wrapOptions = {
           trim: true,
@@ -107,9 +108,9 @@ module.exports = function (options) {
         scope = scope ? '(' + answers.scope.trim() + ')' : '';
 
         // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxSubjectWidth);
 
-        // Wrap these lines at 100 characters
+        // Wrap these lines at 72 characters
         var body = wrap(answers.body, wrapOptions);
 
         // Apply breaking change prefix, removing it if already present
